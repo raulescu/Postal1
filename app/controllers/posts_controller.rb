75f7bd4550e0c8855fa1ae302df53706bf1c049a
raulescu <raul.leul@yahoo.com>
2016-08-11 12:05:25 +0200

    Order posts by creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def index
+     @posts = Post.order( created_at: :desc )
+  end
+
   def create
      post_params = params.require( :post ).permit( :content )
 
