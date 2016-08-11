d38ae195d68654f6e010b0151f3bafa53c2f3b28
raulescu <raul.leul@yahoo.com>
2016-08-11 14:45:02 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
 
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 end
